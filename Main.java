17fadc4385a5fafcb7b50d101120c1e1c0fc1cf7
Hugo Pineiro Eguia <hpineiroeguia@danielcastelao.org>
2021-10-15 10:01:39 +0200

    Arreglo del código + refactor

---
+++ Main.java
@@ -0,0 +1,8 @@
+package clonacionMartaarriba;
+
+public class Main {
+
+    public static void main(String[] args) {
+	System.out.print("Este repositorio era de Marta Pérez");
+    }
+}
---
+++ ejercicio1.java
@@ -0,0 +1,8 @@
+package clonacionMartaarriba;
+
+public class ejercicio1 {
+    public static void main(String[] args) {
+
+        System.out.println("Hola Ej 1 Boletin 1");
+    }
+}
